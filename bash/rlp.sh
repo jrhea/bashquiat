d9bb8f4475e123a53eb24cd094c06225d52b67ca
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2022-05-18 02:48:51 +0000

    dec_to_bin and finish rlp_encode_len

--- bash/rlp.sh
+++ bash/rlp.sh
@@ -12,10 +12,24 @@ hex_to_dec() {
     printf  "%d" $1
 }
 
+dec_to_bin() {
+    local n bit
+    for (( n=$1 ; n>0 ; n >>= 1 )); do  bit="$(( n&1 ))$bit"; done
+    printf "%s" "$bit" # TODO: should this pad with 0s?
+}
+
 rlp_encode_length() {
     local length=$1
     local offset=$(hex_to_dec $2)
-    printf $(dec_to_hex $(($length + $offset)))
+    if [ $length -lt 56 ]
+    then
+        printf $(dec_to_hex $(($length + $offset)))
+    elif [ $length -lt $((2**62)) ] # TODO: should be 2**64, but BASH doesn't like it
+    then
+        local binary_length=$(dec_to_bin $length)
+        printf $(dec_to_hex $(( ${#binary_length} + $offset + 55 )))$binary_length
+    fi
+    
 }
 
 rlp_encode_str() {
@@ -34,5 +48,7 @@ rlp_encode_str() {
 #hex_to_dec 0x7F
 #char_to_hex a
 #rlp_encode_str a
-rlp_encode_str dog
+#rlp_encode_str dog
+rlp_encode_str Comedown.Getoffyourfuckingcross.Weneedthefuckingspacetonailthenextfoolmartyr.
 #rlp_encode_length 3 0x80
+#dec_to_bin 25633445434
