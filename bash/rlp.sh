55253886c63f5417aa0aecb13dd7e78bc80d2035
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2022-05-18 01:32:14 +0000

    dec_to_hex and rlp_encode_length

--- bash/rlp.sh
+++ bash/rlp.sh
@@ -4,8 +4,18 @@ char_to_hex() {
     printf  "0x%x" "'${1}"
 }
 
+dec_to_hex() {
+    printf  "0x%x" $1
+}
+
 hex_to_dec() {
-    printf  "%d" "$1"
+    printf  "%d" $1
+}
+
+rlp_encode_length() {
+    local length=$1
+    local offset=$(hex_to_dec $2)
+    printf $(dec_to_hex $(($length + $offset)))
 }
 
 rlp_encode_str() {
@@ -15,10 +25,14 @@ rlp_encode_str() {
     if [ $length -eq 1 ] && [ $(hex_to_dec $input_hex) -lt $(hex_to_dec 0x80) ]
     then
         printf $input
+    else
+        printf $(rlp_encode_length $length 0x80)$input
     fi
     
 }
 
 #hex_to_dec 0x7F
 #char_to_hex a
-rlp_encode_str a
+#rlp_encode_str a
+rlp_encode_str dog
+#rlp_encode_length 3 0x80
