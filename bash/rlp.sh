3d908994f4853c477341cbd822dfb88cfd6ec40e
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2022-05-19 20:12:31 +0000

    style cleanup

--- bash/rlp.sh
+++ bash/rlp.sh
@@ -25,19 +25,19 @@ dec_to_bin() {
 
 rlp_encode_len() {
     local length=$1
-    local offset=$(hex_to_dec $2)
-    if [ $length -lt 56 ]
+    local offset=$(hex_to_dec "$2")
+    if [ "$length" -lt 56 ]
     then
-        printf $(dec_to_hex $(($length + $offset)))
-    elif [ $length -lt $((2**62)) ] # TODO: should be 2**64, but BASH doesn't like it
+        printf $(dec_to_hex $((length + offset)))
+    elif [ "$length" -lt $((2**62)) ] # TODO: should be 2**64, but BASH doesn't like it
     then
         # TODO: There is a more efficient way to do this
-        local length_binary=$(dec_to_bin $length)
+        local length_binary=$(dec_to_bin "$length")
         # Because dec_to_bin() doesn't pad with zeros we ensure that truncating arithmetic rounds 
         # up by adding (denom-1) to the numerator.
         local length_bytes=$(( (${#length_binary}+7)/8 ))
         # 128 (0x80) + 55 (0x37) = 183 (0xb7) + numBytes(string length)
-        printf $(dec_to_hex $(( $offset + 55 + $length_bytes )))$(dec_to_hex $length)
+        printf $(dec_to_hex $(( offset + 55 + length_bytes )))$(dec_to_hex "$length")
     else
         exit 1
     fi
@@ -47,23 +47,23 @@ rlp_encode_len() {
 rlp_encode_str() {
     local input=$1
     local length=$2
-    if [ $length -eq 1 ] && [ $(hex_to_dec $(char_to_hex "$input")) -lt $(hex_to_dec 0x80) ]
+    if [ "$length" -eq 1 ] && [ $(hex_to_dec $(char_to_hex "$input")) -lt $(hex_to_dec 0x80) ]
     then
         printf $(char_to_hex "$input")
     else
 
-        printf $(rlp_encode_len $length 0x80)$(str_to_hex "$input")
+        printf $(rlp_encode_len "$length" 0x80)$(str_to_hex "$input")
     fi
 }
 
 rlp_encode_int() {
     local input=$1
     local length=$2
-    if [ $length -eq 1 ] && [ $(hex_to_dec "$input") -lt $(hex_to_dec 0x80) ]
+    if [ "$length" -eq 1 ] && [ $(hex_to_dec "$input") -lt $(hex_to_dec 0x80) ]
     then
         printf 0$(dec_to_hex "$input")
     else
-        local input_hex=$(dec_to_hex $input)
+        local input_hex=$(dec_to_hex "$input")
         local length_bytes=$(( (${#input_hex}+1)/2 ))
         printf $(rlp_encode_len $length_bytes 0x80)0$(dec_to_hex "$input")
     fi
@@ -79,20 +79,20 @@ rlp_encode_list() {
     do
         if [ "${input:$i:1}" == "[" ]
         then
-            ((count=$count + 1))
+            ((count=count + 1))
         elif [ "${input:$i:1}" == "]" ]
         then
-            ((count=$count - 1))
+            ((count=count - 1))
         elif [ "${input:$i:1}" == "," ] && [ $count -eq 0 ]
         then
             # replace the character in the ith position with a new delimiter that we can split on 
-            input=$(printf $input | sed s/./\|/$((i + 1)))
+            input=$(printf "$input" | sed s/./\|/$((i + 1)))
         fi
     done
 
     IFS='|' read -r -a items <<< "$input"
     for item in "${items[@]}"; do
-        array+=($(rlp_encode $item))
+        array+=($(rlp_encode "$item"))
     done
     # flatten array into result
     printf -v result '%s' "${array[@]}" # TODO: not a fan of using the extra variable here
@@ -103,25 +103,18 @@ rlp_encode_list() {
 rlp_encode() {
     local input=$1
     local length=${#input}
-    if [ "${input:0:1}" == "[" ] && [ "${input:$(($length-1)):1}" == "]" ]
+    if [ "${input:0:1}" == "[" ] && [ "${input:$((length-1)):1}" == "]" ]
     then
         # remove outer brackets
-        rlp_encode_list "${input:1:$(($length-2))}"
+        rlp_encode_list "${input:1:$((length-2))}"
     elif [ "$input" -eq "$input" ] 2> /dev/null # True if $input is algebraically equal
     then
-        rlp_encode_int "$input" $length
+        rlp_encode_int "$input" "$length"
     else
-        rlp_encode_str "$input" $length
+        rlp_encode_str "$input" "$length"
     fi
 }
 
-
-
-#hex_to_dec 0x7F
-#char_to_hex " "
-#str_to_hex " "
-#rlp_encode_len 3 0x80
-#dec_to_bin 25633445434
 
 rlp_encode " "
 printf '\n%s\n' "20"
